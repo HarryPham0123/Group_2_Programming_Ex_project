8e7bdfa4a1911c2417649cb7c072d0575b14a484
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-03-19 11:28:48 +0700

    update: database configuration

--- server/src/main/java/routes/HomeServlet.java
+++ server/src/main/java/routes/HomeServlet.java
@@ -1,14 +1,30 @@
 package routes;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import model.User;
 
-public class HomeServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.println("Hello world");
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/home")
+public class HomeServlet {
+    private static final Map<Integer, User> USERS = new HashMap<>();
+    static {
+        // Create dummy data
+        for (int i = 1; i <= 10; i++) { USERS.put(i, new User(i, "dummy " + i)); } }
+        private int generateUniqueId() { return USERS.keySet().stream().max((x1, x2) -> x1 - x2).orElse(0) + 1;
+    }
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public List<User> getAll() {
+        return new ArrayList<>(USERS.values());
     }
 }
