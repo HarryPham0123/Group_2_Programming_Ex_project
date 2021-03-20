33aef0c9b257276b3fc6f45fb395d5fbddb49a55
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-03-20 22:57:23 +0700

    Data added + Typo fix + a change in model
    **- Module no FK Program -> relationship M-N 3-ary**
    - Semester in academic year: to show there are years with semesters and faculty but no modules (all in accordance with to Faculty_in_AY)
    - Class: there are modules with no classes and modules with multiple classes
    - Class_has_Lecturer: a lecturer can have 0/1/many class(es) + a class can be taught by 0/1/many lecturer(s)
    - Questionnaire: there may be Classes + Lecturers that haven't filled the form
    - Typo fix: questionaire -> questionnaire (double 'n')

--- Database/databaseConfig.sql
+++ Database/databaseConfig.sql
@@ -63,9 +63,7 @@ DROP TABLE IF EXISTS Module ;
 CREATE TABLE IF NOT EXISTS Module (
   Mcode VARCHAR(10) NOT NULL,
   Mname VARCHAR(50) NULL,
-  Pcode VARCHAR(10) NULL,
-  PRIMARY KEY (Mcode), 
-  FOREIGN KEY (Pcode) REFERENCES Program (Pcode)
+  PRIMARY KEY (Mcode)
 )
 ENGINE = InnoDB;
 
@@ -100,11 +98,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table Questionaire
+-- Table questionnaire
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS Questionaire ;
+DROP TABLE IF EXISTS questionnaire ;
 
-CREATE TABLE IF NOT EXISTS Questionaire (
+CREATE TABLE IF NOT EXISTS questionnaire (
   Ccode VARCHAR(10) NOT NULL,
   Lcode VARCHAR(10) NOT NULL,
   PRIMARY KEY (Ccode, Lcode),
@@ -195,7 +193,7 @@ INSERT INTO Program (Pcode, Pname, Fcode) VALUES (
 	('P493A', 'Architecture', 'F023I'),
 	('P603C', 'Civil Engineering', 'F023I')
 );
-INSERT INTO Module (Mcode, Mname, Pcode) VALUES (
+INSERT INTO Module (Mcode, Mname) VALUES (
 	('M015A', 'Introductory Accounting'),
     ('M005F', 'Introductory Finance'),
     ('M235F', 'Investment Finance'),
@@ -244,4 +242,104 @@ INSERT INTO Faculty_in_AY (AYCode, Fcode) VALUES (
 	('A705C', 'F546E'),
     ('A266Y', 'F546E'),
     ('A332H', 'F023I')
-)
+);
+INSERT INTO Semester (Scode, AYCode) VALUES (
+	('S736o', 'A441C'),
+	('S507q', 'A441C'),
+	('S497g', 'A660A'),
+	('S435x', 'A660A'),
+	('S206p', 'A705C'),
+	('S413z', 'A705C'),
+	('S037x', 'A266Y'),
+	('S241x', 'A266Y'),
+	('S688c', 'A332H'),
+	('S594y', 'A332H')
+);
+INSERT INTO Class (Ccode, size, Scode, Mcode) VALUES (
+	('C161t', '35', 'S736o', 'M539A'),
+	('C889h', '40', 'S413z', 'M426D'),
+	('C406a', '32', 'S507q', 'M426D'),
+	('C507u', '33', 'S206p', 'M015A'),
+	('C546y', '33', 'S206p', 'M539A'),
+	('C905q', '35', 'S736o', 'M516I'),
+	('C722u', '35', 'S413z', 'M154R'),
+	('C188i', '32', 'S206p', 'M834M'),
+	('C990z', '38', 'S736o', 'M005F'),
+	('C363x', '33', 'S206p', 'M256C'),
+	('C383d', '30', 'S413z', 'M256C'),
+	('C296q', '31', 'S507q', 'M426D'),
+	('C130m', '37', 'S413z', 'M834M'),
+	('C556n', '37', 'S206p', 'M923E'),
+	('C256c', '39', 'S206p', 'M426D'),
+	('C197q', '37', 'S206p', 'M923E'),
+	('C387d', '38', 'S413z', 'M426D'),
+	('C817z', '34', 'S507q', 'M426D'),
+	('C973f', '40', 'S206p', 'M923E'),
+	('C400a', '34', 'S507q', 'M834M'),
+	('C443o', '38', 'S413z', 'M351B'),
+	('C311p', '36', 'S736o', 'M351B'),
+	('C958f', '40', 'S206p', 'M834M'),
+	('C791m', '40', 'S206p', 'M351B'),
+	('C732m', '37', 'S413z', 'M834M')
+);
+INSERT INTO Lecturer (Lcode, Lname) VALUES (
+	('L668v', 'Lona Perrigan'),
+	('L322o', 'Marian Sirois'),
+	('L030r', 'Izora Dickerson'),
+	('L500v', 'Sophia Frankel'),
+	('L893a', 'Octavia Mariska'),
+	('L824e', 'Rella Gruis'),
+	('L515u', 'Nona Tineo'),
+	('L960h', 'Anna Adkison'),
+	('L436f', 'Gwendolyn Edwards'),
+	('L192m', 'Lou Hinds'),
+	('L876g', 'Floy Blanco'),
+	('L664m', 'Tena Mee'),
+	('L155l', 'Mabelle Fusco'),
+	('L768a', 'Dicie Mcbride'),
+	('L405m', 'Theresa Buhmann')
+);
+INSERT INTO Class_has_Lecturer (Ccode, Lcode) VALUES (
+	('C161t', 'L668v'),
+	('C889h', 'L030r'),
+	('C406a', 'L030r'),
+	('C507u', 'L322o'),
+	('C546y', 'L824e'),
+	('C905q', 'L960h'),
+	('C722u', 'L436f'),
+	('C188i', 'L322o'),
+	('C990z', 'L876g'),
+	('C363x', 'L960h'),
+	('C383d', 'L515u'),
+	('C296q', 'L192m'),
+	('C130m', 'L155l'),
+	('C556n', 'L668v'),
+	('C256c', 'L192m'),
+	('C197q', 'L664m'),
+	('C387d', 'L893a'),
+	('C817z', 'L876g'),
+	('C973f', 'L515u'),
+	('C400a', 'L876g'),
+	('C443o', 'L030r'),
+	('C311p', 'L155l'),
+    ('C311p', 'L436f'),
+	('C958f', 'L893a'),
+	('C958f', 'L192m')
+);
+INSERT INTO Questionnaire (Ccode, Lcode) VALUES (
+	('C363x', 'L960h'),
+	('C383d', 'L515u'),
+	('C296q', 'L192m'),
+	('C130m', 'L155l'),
+	('C556n', 'L668v'),
+	('C256c', 'L192m'),
+	('C197q', 'L664m'),
+	('C387d', 'L893a'),
+	('C817z', 'L876g'),
+	('C973f', 'L515u'),
+	('C400a', 'L876g'),
+	('C443o', 'L030r'),
+	('C311p', 'L155l'),
+    ('C311p', 'L436f'),
+	('C958f', 'L893a')
+);
