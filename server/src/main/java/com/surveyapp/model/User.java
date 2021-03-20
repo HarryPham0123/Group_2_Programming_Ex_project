2d9af39dc1f65f728d5c5c0e1146a3d2e4c6cfad
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-03-20 10:36:17 +0700

    update: implement lombok

--- server/src/main/java/com/surveyapp/model/User.java
+++ server/src/main/java/com/surveyapp/model/User.java
@@ -1,27 +1,13 @@
 package com.surveyapp.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
     private Integer id;
     private String username;
-
-    public User(int id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
