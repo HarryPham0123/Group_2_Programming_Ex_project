a2144471368a0e855110beb05f050e2d133ff9f0
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-03-22 14:24:24 +0700

    update: database tomcat configuration

--- server/src/main/java/com/surveyapp/routes/HomeServlet.java
+++ server/src/main/java/com/surveyapp/routes/HomeServlet.java
@@ -1,16 +1,21 @@
 package com.surveyapp.routes;
+import com.surveyapp.Configuration;
 import com.surveyapp.model.User;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("/home")
 public class HomeServlet {
@@ -24,6 +29,22 @@ public class HomeServlet {
     @GET
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     public List<User> getAll() {
+        try {
+            Connection connection = Configuration.getAcademiaConnection();
+            if (connection != null) {
+                Statement stm = connection.createStatement();
+                ResultSet res = stm.executeQuery("select * from lecturer");
+                while(res.next()) {
+                    for (int i = 1; i <= res.getMetaData().getColumnCount(); i++) {
+                        System.out.println(res.getObject(i));
+                    }
+                }
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
         return new ArrayList<>(USERS.values());
     }
 }
