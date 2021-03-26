e0b2162f776ecf9dfa0739e79e6443a34f0556c4
viethoang2206 <71742298+viethoang2206@users.noreply.github.com>
2021-03-26 23:26:40 +0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,72 +1,52 @@
-var debug = console.log.bind(console); //Debuger
-
-async function dummydata() {
-    var data = await fetch("http://localhost:8080/api/lecturers")
-    return await data.json();
-}
-
-async function renderDummy() {
-    let lecturers = await dummydata();
-    lecturers.map(lecturer => {
-        var lecturerSelect = document.createElement("option")
-        lecturerSelect.innerHTML = [lecturer.name, lecturer.code].join(" - ");
-        lecturerSelect.value = lecturer.name;
-        document.getElementById("lecturers").append(lecturerSelect)
-    })
-    lecturers.map(lecturer => {
-            var dummyRender = document.getElementById("dummy");
-            var header = document.createElement("h2");
-            header.innerText = lecturer.code;
-            header.className = "lecturer_header";
-            var content = document.createElement("p");
-            content.innerText = lecturer.name;
-            content.className = "lecturer_name";
-            var divCover = document.createElement("div");
-            divCover.className = "lecturer_container";
-            divCover.append(header);
-            divCover.append(content);
-            dummyRender.append(divCover);
-        }
-    )
-    return lecturers
-}
-renderDummy()
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+$(function () {
+    $("body").one("click",".upload", function () {
+        getData();
+    });
 });
-
+$(function () {
+    $("body").on("click",".hide-btn", function () {
+        $(".container").css("display", "none");
+        console.log("a")
+    });
+});
+$(function () {
+    $("body").on("click",".show-btn", function () {
+        $(".lecturers .container").css("display", "block");
+    });
+});
+function getData(){
+    $.ajax({
+		type: 'GET',
+		url: 'mockdata.txt',
+		success: function(data) {
+            //let obj=JSON.stringify(data);
+            let myJson=JSON.parse(data)
+            //console.log(obj[0])
+            console.log(myJson)
+			myJson.map(val=>{
+            // $(`<div class="col-sm-3">
+            //     <h5>Academic Year Code:${val.AYcode}</h5>
+            //     <h5>Faculty code:${val.Fcode}</h5>
+            //     <h5>PM Code:${val.PMcode}</h5>
+            //     <h5>Module code:${val.Mcode}</h5>
+            //     <h5>Program Code:${val.Pcode}</h5>
+            //     <h5>Class code:${val.Ccode}</h5>
+            //     <h5>Size:${val.size}</h5>
+            //     <h5>Semester code:${val.Scode}</h5>
+            //     <h5>Module Name:${val.Mname}</h5>
+            //     <h5>Program name:${val.Pname}</h5>
+            //     <h5>Faculty name:${val.Fname}</h5>
+            //     <h5>FAY code:${val.FAYcode}</h5>
+            // </div>`).appendTo(".row");
+            $(`<tr>
+                <td>${val.AYcode}</td>
+                <td>${val.Ccode}</td>
+                <td>${val.size}</td>
+                <td>${val.Mname}</td>
+                <td>${val.Pname}</td>
+                <td>${val.Fname}</td>
+            <//tr>`).appendTo(".table-list");
+            })
+		}
+	});
+}
