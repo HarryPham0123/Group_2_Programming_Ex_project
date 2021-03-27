5f422aacf69848da77ea0c4b4a96e3b203ae6f19
viethoang2206 <71742298+viethoang2206@users.noreply.github.com>
2021-03-27 14:52:18 +0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -19,34 +19,20 @@ function getData(){
 		type: 'GET',
 		url: 'mockdata.txt',
 		success: function(data) {
-            //let obj=JSON.stringify(data);
-            let myJson=JSON.parse(data)
-            //console.log(obj[0])
-            console.log(myJson)
-			myJson.map(val=>{
-            // $(`<div class="col-sm-3">
-            //     <h5>Academic Year Code:${val.AYcode}</h5>
-            //     <h5>Faculty code:${val.Fcode}</h5>
-            //     <h5>PM Code:${val.PMcode}</h5>
-            //     <h5>Module code:${val.Mcode}</h5>
-            //     <h5>Program Code:${val.Pcode}</h5>
-            //     <h5>Class code:${val.Ccode}</h5>
-            //     <h5>Size:${val.size}</h5>
-            //     <h5>Semester code:${val.Scode}</h5>
-            //     <h5>Module Name:${val.Mname}</h5>
-            //     <h5>Program name:${val.Pname}</h5>
-            //     <h5>Faculty name:${val.Fname}</h5>
-            //     <h5>FAY code:${val.FAYcode}</h5>
-            // </div>`).appendTo(".row");
-            $(`<tr>
-                <td>${val.AYcode}</td>
-                <td>${val.Ccode}</td>
-                <td>${val.size}</td>
-                <td>${val.Mname}</td>
-                <td>${val.Pname}</td>
-                <td>${val.Fname}</td>
-            <//tr>`).appendTo(".table-list");
-            })
-		}
-	});
+            	let myJson=JSON.parse(data)
+		myJson.map(val=>{
+		    $(`<tr>
+			<td>${val.AYcode}</td>
+			<td>${val.Ccode}</td>
+			<td>${val.size}</td>
+			<td>${val.Mname}</td>
+			<td>${val.Pname}</td>
+			<td>${val.Fname}</td>
+		    <//tr>`).appendTo(".table-list");
+		    })
+			},
+	    		error:function () { 
+			alert("Error loading order");
+			}
+		});
 }
