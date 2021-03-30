19a01cb7e55283a22c06193255a188f5fcc8ae95
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2021-03-30 13:44:15 +0700

    Create AcademicYear.java
    Backend - POJO Class - AcademicYear

---
+++ server/src/main/java/com/surveyapp/model/AcademicYear.java
@@ -0,0 +1,15 @@
+package com.surveyapp.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
+public class AcademicYear {
+    @Column(name = "AYcode")
+    private String code;
+}
