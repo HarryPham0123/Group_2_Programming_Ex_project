e9781e6df6faea315e5eeb48dd755e8d24c21617
HuyPC2103 <71742491+HuyPC2103@users.noreply.github.com>
2021-03-30 20:38:19 +0700

    Update ClassDAO.java

--- server/src/main/java/com/surveyapp/service/dao/ClassDAO.java
+++ server/src/main/java/com/surveyapp/service/dao/ClassDAO.java
@@ -7,6 +7,7 @@ import com.surveyapp.model.Semester;
 import com.surveyapp.service.ModuleService;
 import com.surveyapp.service.SemesterService;
 import com.surveyapp.util.DBUtil;
+import com.surveyapp.util.ObjectConverter;
 
 import java.sql.*;
 import java.util.*;
@@ -14,14 +15,8 @@ import java.util.function.Consumer;
 
 public class ClassDAO implements  DAO<Class> {
     private Connection connection = new DBUtil().getConnection();
-    private SemesterService semesterService = new SemesterService();
-    private ModuleService moduleService = new ModuleService();
     private String getAllScript = "SELECT * FROM class";
     private String getByCodeScript = "SELECT * FROM class WHERE Ccode = ?";
-    private String classCodeColumnName = "Ccode";
-    private String classSizeColumnName = "size";
-    private String classSemesterCodeColumnName = "Scode";
-    private String classModuleCodeColumnName = "Mcode";
 
     private void executeInTransaction(Consumer<Connection> action) {
         try {
@@ -45,47 +40,12 @@ public class ClassDAO implements  DAO<Class> {
     }
 
     public List<Class> getAll() {
-        List<Class> classList = new ArrayList<Class>();
+        List<Class> classList = null;
 
-        /*
-         * Retrieve all Academic Years and Modules from database
-         * Convert them into instances of POJO class AcademicYear and Module
-         */
-        List<Semester> semesterList = semesterService.getAll();
-        List<Module> moduleList = moduleService.getAll();
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(getAllScript);
-
-            // Map each instance of Semester POJO to its code
-            Map<String, Semester> semesterCodeMap= new HashMap<String, Semester>();
-            for(int i = 0; i < semesterList.size(); i++) {
-                Semester semester = semesterList.get(i);
-                String semesterCode = semester.getCode();
-                semesterCodeMap.put(semesterCode, semester);
-            }
-
-            // Map each instance of Module POJO to its code
-            Map<String, Module> moduleCodeMap = new HashMap<String, Module>();
-            for(int i = 0; i < moduleList.size(); i++) {
-                Module module = moduleList.get(i);
-                String moduleCode = module.getCode();
-                moduleCodeMap.put(moduleCode, module);
-            }
-
-            while(resultSet.next()) {
-                String classCode = resultSet.getString(classCodeColumnName);
-                int classSize = resultSet.getInt(classSizeColumnName);
-                String classSemesterCode = resultSet.getString(classSemesterCodeColumnName);
-                String classModuleCode = resultSet.getString(classModuleCodeColumnName);
-
-                Semester classSemester = semesterCodeMap.get(classSemesterCode);
-                Module classModule = moduleCodeMap.get(classModuleCode);
-
-                Class aClass = new Class (classCode, classSize, classSemester, classModule);
-                classList.add(aClass);
-            }
-            return classList;
+            classList = (List<Class>) ObjectConverter.toObject(Class.class, resultSet);
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
@@ -98,6 +58,8 @@ public class ClassDAO implements  DAO<Class> {
                 }
             }
         }
+
+        return classList;
     }
 
     @Override
@@ -106,16 +68,7 @@ public class ClassDAO implements  DAO<Class> {
             PreparedStatement preparedStatement = connection.prepareStatement(getByCodeScript);
             preparedStatement.setString(1, code);
             ResultSet resultSet = preparedStatement.executeQuery();
-
-            String classCode = resultSet.getString(classCodeColumnName);
-            int classSize = resultSet.getInt(classSizeColumnName);
-            String classSemesterCode = resultSet.getString(classSemesterCodeColumnName);
-            String classModuleCode = resultSet.getString(classModuleCodeColumnName);
-
-            Semester classSemester = semesterService.get(classSemesterCode);
-            Module classModule = moduleService.get(classModuleCode);
-
-            Class aClass = new Class (classCode, classSize, classSemester, classModule);
+            Class aClass = (Class) ObjectConverter.toObject(Class.class, resultSet);
             return Optional.ofNullable(aClass);
         } catch (Exception exception) {
             exception.printStackTrace();
