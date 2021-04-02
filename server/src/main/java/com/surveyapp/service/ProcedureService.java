adcd76844f2c3299d1632a4496c436a399138d6c
HuyPC2103 <71742491+HuyPC2103@users.noreply.github.com>
2021-04-02 10:42:43 +0700

    Update ProcedureService.java

--- server/src/main/java/com/surveyapp/service/ProcedureService.java
+++ server/src/main/java/com/surveyapp/service/ProcedureService.java
@@ -3,8 +3,37 @@ package com.surveyapp.service;
 import com.surveyapp.service.dao.ProcedureDAO;
 
 public class ProcedureService {
-    private ProcedureDAO procedureDAO = new ProcedureDAO("general_information");
+    private ProcedureDAO procedureDAO = new ProcedureDAO();
+
     public String getAll() {
         return procedureDAO.getAll();
+    }
+
+    public String getAcademicYear(String code) {
+        return procedureDAO.getAcademicYear(code);
+    }
+
+    public String getSemester(String code) {
+        return procedureDAO.getSemester(code);
+    }
+
+    public String getFaculty(String code) {
+        return procedureDAO.getFaculty(code);
+    }
+
+    public String getProgram(String code) {
+        return procedureDAO.getProgram(code);
+    }
+
+    public String getModule(String code) {
+        return procedureDAO.getModule(code);
+    }
+
+    public String getLecturer(String code) {
+        return procedureDAO.getLecturer(code);
+    }
+
+    public String getClass(String code) {
+        return procedureDAO.getClass(code);
     }
 }
