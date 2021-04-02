8f12353ff859defd93645a5c2fdf2d4bfcb8d7cf
HuyPC2103 <71742491+HuyPC2103@users.noreply.github.com>
2021-04-02 10:43:36 +0700

    Update LecturerRoute.java

--- server/src/main/java/com/surveyapp/routes/LecturerRoute.java
+++ server/src/main/java/com/surveyapp/routes/LecturerRoute.java
@@ -1,11 +1,14 @@
-package com.surveyapp.routes;
+package com.surveyapp.controller;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.surveyapp.model.Lecturer;
 import com.surveyapp.service.LecturerService;
 import java.util.List;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Path("/lecturers")
 public class LecturerRoute {
     private LecturerService lecturerService = new LecturerService();
@@ -26,8 +29,23 @@ public class LecturerRoute {
 
     @POST
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public String insert(Lecturer lecturer) {
-        return "Underconstruction!";
+    public Response insert(Lecturer lecturer) {
+        lecturerService.save(lecturer);
+        return Response.ok().entity("New lecturer successfully inserted").build();
+    }
+
+    @PUT
+    @Path("/{code}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response update(@PathParam("code") String code, Lecturer lecturer) {
+        lecturerService.update(code, lecturer);
+        return Response.ok().entity("Successfully updated").build();
+    }
+
+    @DELETE
+    @Path("/{code}")
+    public Response delete(@PathParam("code") String code) {
+        lecturerService.delete(code);
+        return Response.ok().entity("Successfully deleted").build();
     }
 }
