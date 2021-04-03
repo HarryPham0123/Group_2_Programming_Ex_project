adc3a280e3876c563aecb2895919428f34fd7586
HungTran2k <45549446+HungTran2k@users.noreply.github.com>
2021-04-03 18:09:07 +0700

    Update FacultyService.java

--- server/src/main/java/com/surveyapp/service/FacultyService.java
+++ server/src/main/java/com/surveyapp/service/FacultyService.java
@@ -4,10 +4,27 @@ import com.surveyapp.model.Faculty;
 import com.surveyapp.service.dao.FacultyDAO;
 
 import java.util.List;
+import java.util.Optional;
 
 public class FacultyService {
     private FacultyDAO facultyDAO = new FacultyDAO();
     public List<Faculty> getAll() {
         return facultyDAO.getAll();
+    }
+    public Faculty get(String code) {
+        Optional<Faculty> faculty = new FacultyDAO().get(code);
+        return faculty.orElseGet(() -> new Faculty());
+    }
+
+    public void save(Faculty faculty) {
+        facultyDAO.save(faculty);
+    }
+
+    public void update(String code, Faculty faculty) {
+        facultyDAO.update(code, faculty);
+    }
+
+    public void delete(String code) {
+        facultyDAO.delete(code);
     }
 }
