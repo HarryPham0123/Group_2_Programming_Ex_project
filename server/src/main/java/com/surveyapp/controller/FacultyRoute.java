139ceddff9eb47b498b6cb958dcbfbc5e74dafdc
HungTran2k <45549446+HungTran2k@users.noreply.github.com>
2021-04-03 18:07:57 +0700

    Update FacultyRoute.java

--- server/src/main/java/com/surveyapp/controller/FacultyRoute.java
+++ server/src/main/java/com/surveyapp/controller/FacultyRoute.java
@@ -1,6 +1,6 @@
 package com.surveyapp.controller;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.surveyapp.model.Faculty;
 import com.surveyapp.service.FacultyService;
 
@@ -10,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Path("/faculties")
 public class FacultyRoute {
     private FacultyService facultyService = new FacultyService();
@@ -20,4 +21,35 @@ public class FacultyRoute {
         return facultyList;
     }
 
+    @GET
+    @Path("/{code}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Faculty getByCode(@PathParam("code") String code) {
+        Faculty faculty = facultyService.get(code);
+        return faculty;
+    }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response insert(Faculty faculty) {
+        facultyService.save(faculty);
+        return Response.ok().entity("New faculty successfully inserted").build();
+    }
+
+    @PUT
+    @Path("/{code}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response update(@PathParam("code") String code, Faculty faculty) {
+        facultyService.update(code, faculty);
+        return Response.ok().entity("Successfully updated").build();
+    }
+
+    @DELETE
+    @Path("/{code}")
+    public Response delete(@PathParam("code") String code) {
+        facultyService.delete(code);
+        return Response.ok().entity("Successfully deleted").build();
+    }
+
 }
+
