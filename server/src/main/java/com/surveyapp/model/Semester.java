05754c384afb4fe8256ddd2818b1a18bfd90d0eb
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-04-07 00:40:56 +0700

    update/back-end: semester model

--- server/src/main/java/com/surveyapp/model/Semester.java
+++ server/src/main/java/com/surveyapp/model/Semester.java
@@ -5,15 +5,35 @@ import lombok.*;
 
 import javax.persistence.Column;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Setter
-@Getter
-
 public class Semester {
     @Column(name = "Scode")
     private String code;
+
     @Column(name = "AYcode")
     private String AYCode;
+
+    public Semester(String code, String AYCode) {
+        this.code = code;
+        this.AYCode = AYCode;
+    }
+
+    public Semester() {
+        this(null, null);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAYCode() {
+        return AYCode;
+    }
+
+    public void setAYCode(String AYCode) {
+        this.AYCode = AYCode;
+    }
 }
