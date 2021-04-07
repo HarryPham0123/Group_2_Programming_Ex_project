71840ccbad8cc3b7097235580d4264f03c1f58e1
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-04-08 01:26:45 +0700

    update/client: validator, POST questionnaire, html re-structure

--- client/Questionaire/script-final.js
+++ client/Questionaire/script-final.js
@@ -1,3 +1,6 @@
+/*Created by Ha Xuan Huy, Vu Viet Hoang, Nguyen Dang Khoa
+* This script is the implementation of questionnaire page
+* */
 $(function () {
     $("input").empty();
     $("body").one("click",".sel-class", function () {
@@ -7,33 +10,101 @@ $(function () {
          const newVal=getVal();
          addInput(newVal)
          changLec(newVal)
-    }); 
+    });
+
     $('.submit-btn').click(function() {
-        const getQuestion=document.getElementsByClassName("question")
-        if($(".sel-class option:selected").val()=="starter"){
-            alert("Please select the class option")
+        var isNotValid = answerValidator();
+
+        if (isNotValid) {
+            alert(isNotValid);
+            return;
         }
-        else if($(".sel-lec option:selected").val()=="starter"){
-            alert("Please select the lecturer option")
-        }
-        if(!$('input[name=attend]:checked').length > 0){
-            alert("Please fill in your attendance")
-        }
-        if(!$('input[name=gender]:checked').length > 0){
-            alert("Please fill in your gender")
-        }
-        for(let i =1;i<=getQuestion.length;i++){
-            if (!$('input[name=question'+`${i}`+']:checked').length > 0) {
-                alert("Please fill in the question"+`${i}`);
-            }
-        }
-        for(let i =1;i<=getQuestion.length;i++){
-            if (!$('input[name=question'+`${i}`+']:checked').length > 0) {
-                alert("Please fill in the question"+`${i}`); 
-            }
-        }
+
+        submitQuestionnaire();
      });
 });
+
+function answerValidator() {
+    const questions = $(".question");
+    var errorMessage = "";
+
+    if($(".sel-class option:selected").val() == "starter"){
+        errorMessage += "Please select the class option \n";
+    }
+
+    if($(".sel-lec option:selected").val() == "starter"){
+        errorMessage += "Please select the lecturer option \n";
+    }
+
+    if(!$('input[name=attend]:checked')){
+        errorMessage += "Please fill in your attendance \n";
+    }
+
+    if(!$('input[name=gender]:checked')){
+        errorMessage += "Please fill in your gender \n";
+    }
+
+    for(let i = 1 ; i <= questions.length; i++) {
+        if ($(`input[name=question${i}]:checked`).length == 0) {
+            errorMessage += `Please fill in the question ${i} \n`;
+        }
+    }
+
+    return errorMessage;
+}
+
+function gatherQuestionAnswer() {
+    const questions = $('.question');
+    var questionList = [];
+
+    //Attendance question
+    questionList.push({
+        "question": "attendance",
+        "answer": String($("input[name=attendance]:checked").val())
+    });
+
+    //Gender question
+    questionList.push({
+        "question": "gender",
+        "answer": String($("input[name=gender]:checked").val())
+    });
+
+    //Questions
+    for(let i = 1; i <= questions.length ; i++){
+        questionList.push({
+            "question": String(i),
+            "answer": parseInt($(`input[name=question${i}]:checked`).val())
+        })
+    }
+
+    //Additional comment session
+    questionList.push({
+        "question": "18",
+        "answer": String($("textarea[name=question18]").val())
+    })
+
+    return questionList;
+}
+function submitQuestionnaire() {
+    var requestBody = {
+        lcode: String($(".sel-class option:selected").val()),
+        ccode: String($(".sel-lec option:selected").val()),
+        question_list: gatherQuestionAnswer()
+    };
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/survey/api/questionnaire",
+        contentType: "application/json",
+        data: JSON.stringify(requestBody),
+        success: function(data) {
+            alert(data.message.string);
+        },
+        error: function(data) {
+            console.log(data.message.string)
+        }
+    })
+    return requestBody;
+}
 function getData(){
     $.ajax({
 		type: 'GET',
@@ -63,7 +134,7 @@ const api='http://localhost:8080/survey/api/general';
             return val.Ccode.includes(input)
         })
             $(".acad-year").val(newData[0].AYcode);
-            $(".semester").val(newData[0].size);
+            $(".semester").val(newData[0].Scode);
             $(".faculty").val(newData[0].Fname);
             $(".module").val(newData[0].Mname);
             $(".program").val(newData[0].Pname);
@@ -84,7 +155,7 @@ function changLec(input){
             })
             newObj.map(value=>{
                 $(`
-                <option>${value.Lname}</option>
+                <option value=${value.Lcode}>${value.Lname}</option>
             `).appendTo(".sel-lec");
             })
         });
