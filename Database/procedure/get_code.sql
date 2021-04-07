23ddc8883b3c8a3e4a9cd8294f07e01383c2d8cd
Harry <48181803+HarryPham0123@users.noreply.github.com>
2021-04-07 09:02:58 +0700

    Add files via upload

---
+++ Database/procedure/get_code.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE `get_code`()
+BEGIN
+	SELECT ay.AYcode,
+		s.Scode,
+        f.Fcode,
+        p.Pcode,
+        m.Mcode,
+        c.Ccode
+	FROM class c
+		NATURAL JOIN lecturer_in_class NATURAL JOIN lecturer lec 
+		NATURAL JOIN semester s
+		NATURAL JOIN academic_year ay
+		NATURAL JOIN module m
+		NATURAL JOIN program_module pm
+		NATURAL JOIN program p
+		NATURAL JOIN ay_faculty_pm 
+		NATURAL JOIN faculty f;
+END
