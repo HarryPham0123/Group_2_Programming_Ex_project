34b7f3d2c322600bb5d32690ca7fc088e594eccd
Harry <48181803+HarryPham0123@users.noreply.github.com>
2021-04-07 08:19:47 +0700

    Update insert_questionnaire.sql

--- Database/procedure/insert_questionnaire.sql
+++ Database/procedure/insert_questionnaire.sql
@@ -9,7 +9,9 @@ BEGIN
 		question_5, question_6, question_7, question_8, question_9, question_10,
 		question_11, question_12, question_13, question_14, question_15, question_16,
 		question_17, question_18) 
-	VALUES (input_Ccode, input_Lcode, question_1_input, question_2_input, question_3_input, question_4_input, question_5_input,
-		question_6_input, question_7_input, question_8_input, question_9_input, question_10_input, question_11_input,
-		question_12_input, question_13_input, question_14_input, question_15_input, question_16_input, question_17_input, question_18_input);
+	VALUES (input_Ccode, input_Lcode, CAST(question_1_input AS NCHAR), CAST(question_2_input AS NCHAR), CAST(question_3_input AS NCHAR), CAST(question_4_input AS NCHAR),
+		CAST(question_5_input AS NCHAR), CAST(question_6_input AS NCHAR), CAST(question_7_input AS NCHAR), CAST(question_8_input AS NCHAR), CAST(question_9_input AS NCHAR), 
+		CAST(question_10_input AS NCHAR), CAST(question_11_input AS NCHAR),
+		CAST(question_12_input AS NCHAR), CAST(question_13_input AS NCHAR), CAST(question_14_input AS NCHAR), CAST(question_15_input AS NCHAR),
+		CAST(question_16_input AS NCHAR), CAST(question_17_input AS NCHAR), CAST(question_18_input AS NCHAR));
 END
