0956348a8f77598ae0164091a422699c4acceb7a
HungTran2k <45549446+HungTran2k@users.noreply.github.com>
2021-04-08 12:58:07 +0700

    Update ProgramRoute.java

--- server/src/main/java/com/surveyapp/controller/ProgramRoute.java
+++ server/src/main/java/com/surveyapp/controller/ProgramRoute.java
@@ -2,10 +2,12 @@ package com.surveyapp.controller;
 import com.surveyapp.model.Program;
 import com.surveyapp.service.program.ProgramService;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/programs")
 public class ProgramRoute {
@@ -15,5 +17,35 @@ public class ProgramRoute {
     public List<Program> getAll() {
         List<Program> programList = programService.getAll();
         return programList;
+    }
+    
+     @GET
+    @Path("/{code}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Program getByCode(@PathParam("code") String code) {
+        Program program = programService.get(code);
+        return program;
+    }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response insert(Program program) {
+        programService.save(program);
+        return Response.ok().entity("New program successfully inserted").build();
+    }
+
+    @PUT
+    @Path("/{code}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response update(@PathParam("code") String code, Program program) {
+        programService.update(code, program);
+        return Response.ok().entity("Successfully updated").build();
+    }
+
+    @DELETE
+    @Path("/{code}")
+    public Response delete(@PathParam("code") String code) {
+        programService.delete(code);
+        return Response.ok().entity("Successfully deleted").build();
     }
 }
