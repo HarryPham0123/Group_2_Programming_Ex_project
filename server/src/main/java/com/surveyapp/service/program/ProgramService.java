a763e6787973d9d7d5649d492aa772ae86f086e4
HungTran2k <45549446+HungTran2k@users.noreply.github.com>
2021-04-08 12:48:28 +0700

    Update ProgramService.java

--- server/src/main/java/com/surveyapp/service/program/ProgramService.java
+++ server/src/main/java/com/surveyapp/service/program/ProgramService.java
@@ -9,4 +9,21 @@ public class ProgramService {
     public List<Program> getAll() {
         return programDAO.getAll();
     }
+    public Program get(String code) {
+        Optional<Program> program = new ProgramDAO().get(code);
+        return program.orElseGet(() -> new Program());
+    }
+
+    public void save(Program program) {
+        programDAO.save(program);
+    }
+
+    public void update(String code, Program program) {
+        programDAO.update(code, program);
+    }
+
+    public void delete(String code) {
+        programDAO.delete(code);
+    }
+    
 }
