ba7c9027ea2049b813ad38e2510b2c16ec4e95b9
HuyPC2103 <huypc1230@prontonmail.com>
2021-04-11 17:20:38 +0700

    Add Route, DAO and Service For summary_gender and summary_attendance

--- server/src/main/java/com/surveyapp/controller/QuestionnaireRoute.java
+++ server/src/main/java/com/surveyapp/controller/QuestionnaireRoute.java
@@ -53,4 +53,48 @@ public class QuestionnaireRoute {
                     .entity(jsonObjectBuilder.build()).build();
         }
     }
+
+    @GET
+    @Path("/attendance_question")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response getSummaryAttendance(
+            @QueryParam("academic_year") String academic_year,
+            @QueryParam("semester") String semester,
+            @QueryParam("faculty") String faculty,
+            @QueryParam("program") String program,
+            @QueryParam("module") String module,
+            @QueryParam("class") String clazz,
+            @QueryParam("lecturer") String lecturer
+    ) {
+        try {
+            Code code = new Code(academic_year, semester, faculty, program, module, lecturer, clazz);
+            return Response.status(Response.Status.OK).entity(procedureService.getSummaryAttendance(code)).build();
+        } catch (Exception exception) {
+            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+            jsonObjectBuilder.add("message", exception.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(jsonObjectBuilder.build()).build();
+        }
+    }
+
+    @GET
+    @Path("/gender_question")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response getSummaryGender(
+            @QueryParam("academic_year") String academic_year,
+            @QueryParam("semester") String semester,
+            @QueryParam("faculty") String faculty,
+            @QueryParam("program") String program,
+            @QueryParam("module") String module,
+            @QueryParam("class") String clazz,
+            @QueryParam("lecturer") String lecturer
+    ) {
+        try {
+            Code code = new Code(academic_year, semester, faculty, program, module, lecturer, clazz);
+            return Response.status(Response.Status.OK).entity(procedureService.getSummaryGender(code)).build();
+        } catch (Exception exception) {
+            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+            jsonObjectBuilder.add("message", exception.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(jsonObjectBuilder.build()).build();
+        }
+    }
 }
