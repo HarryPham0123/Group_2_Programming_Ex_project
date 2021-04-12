d6b14426616046c70f98bca524379f14f72b5dae
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-12 09:22:53 +0700

    VARCHAR to INT

--- Database/procedure/insert_questionnaire.sql
+++ Database/procedure/insert_questionnaire.sql
@@ -1,8 +1,8 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_questionnaire`(input_Ccode VARCHAR(10), input_Lcode VARCHAR(10), input_gender VARCHAR(10), input_attendance VARCHAR(10),
-					question_1_input Varchar(10), question_2_input Varchar(10), question_3_input Varchar(10),
-				       question_4_input Varchar(10), question_5_input Varchar(10), question_6_input Varchar(10), question_7_input Varchar(10), question_8_input Varchar(10), 
-					question_9_input Varchar(10), question_10_input Varchar(10), question_11_input Varchar(10), question_12_input Varchar(10), question_13_input Varchar(10),
-					question_14_input Varchar(10), question_15_input Varchar(10), question_16_input Varchar(10), question_17_input Varchar(10), question_18_input Varchar(10))
+					question_1_input INT, question_2_input INT, question_3_input INT,
+				       question_4_input INT, question_5_input INT, question_6_input INT, question_7_input INT, question_8_input INT, 
+					question_9_input INT, question_10_input INT, question_11_input INT, question_12_input INT, question_13_input INT,
+					question_14_input INT, question_15_input INT, question_16_input INT, question_17_input INT, question_18_input Varchar(100))
 BEGIN
 IF EXISTs (SELECT lc.Ccode, lc.Lcode
 			FROM lecturer_in_class lc
@@ -21,4 +21,3 @@ IF EXISTs (SELECT lc.Ccode, lc.Lcode
     question_13_input , question_14_input , question_15_input , question_16_input , question_17_input , question_18_input);
 END IF;
 END
-
