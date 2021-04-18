879ea40b6dcae7c11916af0996ee0e253006c104
HuyPC2103 <huypc1230@prontonmail.com>
2021-04-18 10:14:05 +0700

    Add files for Authorisation

---
+++ server/src/main/java/com/surveyapp/service/Login_test_Cong_Huy/LoginCredential.java
@@ -0,0 +1,15 @@
+package com.surveyapp.service.Login_test_Cong_Huy;
+
+public class LoginCredential {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
