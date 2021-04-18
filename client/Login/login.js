45c2562594b2eaf2f3bc977a1817c3a2e88a1764
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2021-04-18 11:50:04 +0700

    Create login.js

---
+++ client/Login/login.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    $(".login-btn").click(function() {
+        const usernamevalid=$('#username').val();
+        const passwordvalid=$('#password').val();
+        if(usernamevalid.length == 0 || passwordvalid.length == 0){
+            alert("Please provide all the input fields before login");
+        }
+        else{
+            let PostJson = {
+                "username" : usernamevalid,
+                "password" : passwordvalid,
+            }
+            $.ajax({
+                type: 'POST',
+                contentType: "application/json",
+                url: "http://localhost:8080/survey/api/auth",
+                data: JSON.stringify(PostJson),
+                dataType: "text",
+                error: function(e) {
+                    alert("Something wrong");
+                    console.log(e);
+                },
+                success : function(data){
+                    alert("Login successfully");
+                    window.location.replace("/survey/Questionaire/questionaire-final-updated.html");
+                }
+            })
+        }
+    })
+})
