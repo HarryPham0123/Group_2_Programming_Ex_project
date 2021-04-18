cf0b6a3e6380a8902d4e677a75dd858578b467c7
HungTran2k <45549446+HungTran2k@users.noreply.github.com>
2021-04-18 10:56:07 +0700

    Update LecturerDAO.java

--- server/src/main/java/com/surveyapp/service/lecturer/LecturerDAO.java
+++ server/src/main/java/com/surveyapp/service/lecturer/LecturerDAO.java
@@ -1,136 +1,85 @@
-package com.surveyapp.service.lecturer;
-
+package com.surveyapp.controller;
 import com.surveyapp.model.Lecturer;
-import com.surveyapp.service.DAO;
-import com.surveyapp.util.DBUtil;
-import com.surveyapp.util.ObjectConverter;
+import com.surveyapp.service.lecturer.LecturerDAO;
+import com.surveyapp.service.lecturer.LecturerService;
 
-import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
 
-public class LecturerDAO implements DAO<Lecturer> {
-    private Connection connection = new DBUtil().getConnection();
-    private String getAllScript = "SELECT * FROM lecturer";
-    private String getByCodeScript = "SELECT * FROM lecturer WHERE Lcode = ?";
-    private String saveScript = "INSERT INTO lecturer (Lcode, Lname) VALUES(?, ?)";
-    private String updateScript = "UPDATE lecturer SET Lcode = ?, Lname = ? WHERE Lcode = ?";
-    private String deleteScript = "DELETE FROM lecturer WHERE  Lcode = ?";
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-    private void executeInTransaction(Consumer<Connection> action) throws Exception{
-        try {
-            connection.setAutoCommit(false);
-            action.accept(connection);
-            connection.commit();
-        } catch (SQLException sqlException) {
-            try {
-                connection.rollback();
-            } catch (SQLException exception) {
-                exception.printStackTrace();
-            }
-            sqlException.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-            }
-        }
-    }
 
-    @Override
-    public List<Lecturer> getAll()throws Exception {
-        List<Lecturer> lecturerList = null;
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(getAllScript);
-            lecturerList = (List<Lecturer>) ObjectConverter.toObject(Lecturer.class, resultSet);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        } finally {
-            if (connection != null)  {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
+@Path("/lecturers")
+public class LecturerRoute {
+    private LecturerService lecturerService = new LecturerService();
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public List<Lecturer> getAll() {
+        List<Lecturer> lecturerList = new ArrayList<>();
+        try{
+            lecturerList = lecturerService.getAll();
+            return lecturerList;
+        }catch(Exception exception){
+            System.out.println(exception);
         }
         return lecturerList;
     }
 
-    @Override
-    public Optional<Lecturer> get(String code)throws Exception {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(getByCodeScript);
-            preparedStatement.setString(1, code);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            Lecturer lecturer = (Lecturer) ObjectConverter.toObject(Lecturer.class, resultSet);
-            return Optional.ofNullable(lecturer);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public void save(Lecturer lecturer) throws Exception{
+    @GET
+    @Path("/{code}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Lecturer getByCode(@PathParam("code") String code) {
+        Lecturer lecturer = new Lecturer();
         try{
-            PreparedStatement preparedStatement = connection.prepareStatement(saveScript);
-            preparedStatement.setString(1, lecturer.getCode());
-            preparedStatement.setString(2, lecturer.getName());
-            preparedStatement.executeUpdate();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        } finally {
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            }
+            lecturer = lecturerService.get(code);
+            return lecturer;
+        }catch(Exception exception){
+            System.out.println(exception);
+        }
+        return lecturer;
+    }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response insert(Lecturer lecturer) {
+        try{
+            lecturerService.save(lecturer);
+            return Response.status(Response.Status.OK).build();
+        }catch(Exception exception){
+            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+            jsonObjectBuilder.add("message", exception.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(jsonObjectBuilder.build()).build();
         }
     }
 
-    @Override
-    public void update(String code, Lecturer lecturer) throws Exception {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(updateScript);
-            preparedStatement.setString(1, lecturer.getCode());
-            preparedStatement.setString(2, lecturer.getName());
-            preparedStatement.setString(3, code);
-            preparedStatement.executeUpdate();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        } finally  {
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            }
+    @PUT
+    @Path("/{code}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response update(@PathParam("code") String code, Lecturer lecturer){
+        try{
+            lecturerService.update(code, lecturer);
+            return Response.status(Response.Status.OK).build();
+        }catch(Exception exception){
+            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+            jsonObjectBuilder.add("message", exception.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(jsonObjectBuilder.build()).build();
         }
     }
 
-    @Override
-    public void delete(String code) throws Exception {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(deleteScript);
-            preparedStatement.setString(1, code);
-            preparedStatement.executeUpdate();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        } finally {
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch(Exception exception) {
-                    exception.printStackTrace();
-                }
-            }
+    @DELETE
+    @Path("/{code}")
+    public Response delete(@PathParam("code") String code) {
+        try{
+            lecturerService.delete(code);
+            return Response.status(Response.Status.OK).build();
+        }catch(Exception exception){
+            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+            jsonObjectBuilder.add("message", exception.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(jsonObjectBuilder.build()).build();
         }
     }
 }
