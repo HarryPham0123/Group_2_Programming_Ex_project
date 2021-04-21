d8a9f483e58d85fcaf24885b9dcbcd70572f453f
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-22 00:52:27 +0700

    Update join tables and errors handling

--- Database/procedure/general_information.sql
+++ Database/procedure/general_information.sql
@@ -2,7 +2,7 @@ USE pe2018;
 DROP PROCEDURE IF EXISTS general_information;
 DELIMITER //
 CREATE PROCEDURE `general_information`(
-	input_academic_year VARCHAR(10), 
+	input_academic_year VARCHAR(9), 
 	input_semester VARCHAR(10), 
 	input_faculty VARCHAR(10), 
 	input_program VARCHAR(10), 
@@ -11,56 +11,56 @@ CREATE PROCEDURE `general_information`(
 	input_class VARCHAR(10))
 sp: BEGIN
 -- Check invalid parameter (Para NOT null but not in database)
+-- If invalid, stop the procedure and output the error message
 CASE
 	WHEN (input_academic_year not in (Select AYcode from academic_year)) AND (input_academic_year is not NULL) THEN
-		SELECT 'Invalid academic year' as 'Error_message';
+		SELECT 'invalid academic year' as 'message';
         LEAVE sp;
         
 	WHEN (input_semester not in (Select Scode from semester)) AND (input_semester is not NULL) THEN
-		SELECT 'Invalid semester' as 'Error_message';
+		SELECT 'invalid semester' as 'message';
         LEAVE sp;
 
 	WHEN (input_faculty not in (Select Fcode from faculty)) AND (input_faculty is not NULL) THEN
-		SELECT 'Invalid faculty' as 'Error_message';
+		SELECT 'invalid faculty' as 'message';
         LEAVE sp;
 
 	WHEN (input_program not in (Select Pcode from program)) AND (input_program is not NULL) THEN
-		SELECT 'Invalid program' as 'Error_message';
+		SELECT 'invalid program' as 'message';
         LEAVE sp;
 
 	WHEN (input_module not in (Select Mcode from module)) AND (input_module is not NULL) THEN
-		SELECT 'Invalid module' as 'Error_message';
+		SELECT 'invalid module' as 'message';
         LEAVE sp;
 
 	WHEN (input_lecturer not in (Select Lcode from lecturer)) AND (input_lecturer is not NULL) THEN
-		SELECT 'Invalid lecturer' as 'Error_message';
+		SELECT 'invalid lecturer' as 'message';
         LEAVE sp;
         
 	WHEN (input_class not in (Select Ccode from class)) AND (input_class is not NULL) THEN
-		SELECT 'Invalid class' as 'Error_message';
+		SELECT 'invalid class' as 'message';
         LEAVE sp;
+	-- In the case of all parameters are valid
 	ELSE
--- Query for getting the general information
-SELECT ay.AYcode as 'Academic Year code',
+-- Query to get the general information
+SELECT ay.AYcode as 'Academic_Year_code',
 	s.Scode as 'Semester',
 	f.Fcode as 'Faculty',
 	p.Pcode as 'Program',
 	m.Mcode as 'Module',
-	c.Ccode as 'Class code',
-    lec.Lcode as 'Lecturer code',
-	c.size as 'Class size'
+	c.Ccode as 'Class_code',
+    lec.Lcode as 'Lecturer_code',
+	c.size as 'Class_size'
 FROM class c
    NATURAL JOIN lecturer_in_class NATURAL JOIN lecturer lec 
    NATURAL JOIN semester s
    NATURAL JOIN academic_year ay
-   NATURAL JOIN module m
-   NATURAL JOIN program_module 
-   NATURAL JOIN program p
-   NATURAL JOIN ay_faculty_pm 
-   NATURAL JOIN faculty f
+   NATURAL JOIN ay_fac NATURAL JOIN faculty f
+   NATURAL JOIN ay_fac_p NATURAL JOIN program p
+   NATURAL JOIN ay_fac_pm NATURAL JOIN module m
 
 WHERE 
--- Check if parameter NULL or NOT, if yes, query based on the other parameters
+-- Check if parameter NULL or NOT, if yes, query based on the other parameters (filter)
 	( input_academic_year is null
 		or ay.AYcode = input_academic_year
 	)
