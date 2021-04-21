3e640c0e1ac2f6f3dd4d74e2611a8fd2bfebef88
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-22 00:59:15 +0700

    Update errors handling

--- Database/procedure/insert_questionnaire.sql
+++ Database/procedure/insert_questionnaire.sql
@@ -1,4 +1,8 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_questionnaire`(input_Ccode VARCHAR(10), input_Lcode VARCHAR(10),  input_attendance VARCHAR(10), input_gender VARCHAR(10),
+USE pe2018;
+DROP PROCEDURE IF EXISTS insert_questionnaire;
+
+DELIMITER //
+CREATE PROCEDURE `insert_questionnaire`(input_Ccode VARCHAR(10), input_Lcode VARCHAR(10),  input_attendance VARCHAR(10), input_gender VARCHAR(10),
 					question_1_input Varchar(10), question_2_input Varchar(10), question_3_input Varchar(10),
 				       question_4_input Varchar(10), question_5_input Varchar(10), question_6_input Varchar(10), question_7_input Varchar(10), question_8_input Varchar(10), 
 					question_9_input Varchar(10), question_10_input Varchar(10), question_11_input Varchar(10), question_12_input Varchar(10), question_13_input Varchar(10),
@@ -19,7 +23,8 @@ IF EXISTs (SELECT lc.Ccode, lc.Lcode
     question_1_input , question_2_input, question_3_input , question_4_input , question_5_input , question_6_input , 
     question_7_input , question_8_input , question_9_input , question_10_input , question_11_input , question_12_input , 
     question_13_input , question_14_input , question_15_input , question_16_input , question_17_input , question_18_input);
-    SELECT 'Success' as 'Outcome';
-ELSE SELECT 'Failure' as 'Outcome';
+    SELECT 'success' as 'message';
+ELSE SELECT 'failure' as 'message';
 END IF;
 END
+//
