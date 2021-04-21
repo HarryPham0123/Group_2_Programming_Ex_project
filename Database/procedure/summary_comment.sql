2a63fc28e33de69ad4776c42f74ad3ec0262aee8
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-22 01:02:46 +0700

    Query comments (Question_18)

--- Database/procedure/summary_comment.sql
+++ Database/procedure/summary_comment.sql
@@ -1,15 +1,24 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `summary_comment`(input_Ccode VARCHAR(10), input_Lcode VARCHAR(10))
+USE pe2018;
+DROP PROCEDURE IF EXISTS summary_comment;
+
+DELIMITER //
+CREATE PROCEDURE `summary_comment`(input_Ccode VARCHAR(10), input_Lcode VARCHAR(10))
 BEGIN
+-- Check if the pair class_code and lecturer_code exists in DB (the class is taught by the lecturer or not)
+-- If yes, then query all corresponding comments
 IF EXISTs (SELECT lc.Ccode, lc.Lcode
 			FROM lecturer_in_class lc
             WHERE lc.Ccode = input_Ccode
             AND lc.Lcode = input_Lcode
             )
 	THEN
-		SELECT question_18 as 'Comment'
+		SELECT question_18 as 'comment'
         FROM questionnaire q
         WHERE q.Ccode = input_Ccode
             AND q.Lcode = input_Lcode;
-ELSE SELECT 'No' as 'valid';
+            
+-- Else, return error message
+ELSE SELECT 'no' as 'message';
 END IF;
 END
+//
