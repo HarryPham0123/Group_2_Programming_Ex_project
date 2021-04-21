2fa08b2f00231b69eec4c7514fa03912b457f2ce
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-22 00:56:18 +0700

    Update join table

--- Database/procedure/get_code.sql
+++ Database/procedure/get_code.sql
@@ -3,6 +3,8 @@ DROP PROCEDURE IF EXISTS get_code;
 DELIMITER //
 CREATE PROCEDURE `get_code`()
 BEGIN
+-- Join all to create a big table
+-- Then return all information
 	SELECT ay.AYcode,
 		s.Scode,
         f.Fcode,
@@ -12,13 +14,12 @@ BEGIN
         lec.Lcode,
         c.size
 	FROM class c
-		NATURAL JOIN lecturer_in_class NATURAL JOIN lecturer lec 
-		NATURAL JOIN semester s
-		NATURAL JOIN academic_year ay
-		NATURAL JOIN module m
-		NATURAL JOIN program_module pm
-		NATURAL JOIN program p
-		NATURAL JOIN ay_faculty_pm 
-		NATURAL JOIN faculty f;
+   NATURAL JOIN lecturer_in_class NATURAL JOIN lecturer lec 
+   NATURAL JOIN semester s
+   NATURAL JOIN academic_year ay
+   NATURAL JOIN ay_fac NATURAL JOIN faculty f
+   NATURAL JOIN ay_fac_p NATURAL JOIN program p
+   NATURAL JOIN ay_fac_pm NATURAL JOIN module m
+;
 END
 //
