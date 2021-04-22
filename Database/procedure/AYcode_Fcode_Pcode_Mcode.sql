b35d78c23053747c2452bc02b0117ef42f056b2b
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-22 11:58:30 +0700

    Formatting

--- Database/procedure/AYcode_Fcode_Pcode_Mcode.sql
+++ Database/procedure/AYcode_Fcode_Pcode_Mcode.sql
@@ -13,19 +13,19 @@ sp: BEGIN
 -- Check DATA EXIST when DELETE case
 	CASE
 		-- Check invalid parameter (Para NOT null but not in database)
-		WHEN (AYcode_input not in (Select AYcode from academic_year)) AND (AYcode_input is not NULL) THEN
+		WHEN (input_AYcode not in (Select AYcode from academic_year)) AND (input_AYcode is not NULL) THEN
 			SELECT 'invalid academic year' as 'message';
 			LEAVE sp;
 
-		WHEN (Fcode_input not in (Select Fcode from faculty)) AND (Fcode_input is not NULL) THEN
+		WHEN (input_Fcode not in (Select Fcode from faculty)) AND (input_Fcode is not NULL) THEN
 			SELECT 'invalid faculty' as 'message';
 			LEAVE sp;
 
-		WHEN (Pcode_input not in (Select Pcode from program)) AND (Pcode_input is not NULL) THEN
+		WHEN (input_Pcode not in (Select Pcode from program)) AND (input_Pcode is not NULL) THEN
 			SELECT 'invalid program' as 'message';
 			LEAVE sp;
 
-		WHEN (Mcode_input not in (Select Mcode from module)) AND (Mcode_input is not NULL) THEN
+		WHEN (input_Mcode not in (Select Mcode from module)) AND (input_Mcode is not NULL) THEN
 			SELECT 'invalid module' as 'message';
 			LEAVE sp;	
 		
