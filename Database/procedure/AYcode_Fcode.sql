dd0983fcb213885f63116e570541e8869fd14606
14923CS2018 <71810384+14923CS2018@users.noreply.github.com>
2021-04-23 15:14:52 +0700

    Change parameters name

--- Database/procedure/AYcode_Fcode.sql
+++ Database/procedure/AYcode_Fcode.sql
@@ -3,30 +3,30 @@ DROP PROCEDURE IF EXISTS AYcode_Fcode;
 
 DELIMITER //
 CREATE PROCEDURE `AYcode_Fcode`(
-	input_AYcode Varchar(9),
-    input_Fcode Varchar(10),
+	input_academic_year Varchar(9),
+    input_faculty Varchar(10),
     status_case Varchar(50))
 sp: BEGIN
 	CASE 
 		-- Check invalid parameter (Para NOT null but not in database)
-		WHEN (input_AYcode not in (Select AYcode from academic_year)) AND (input_AYcode is not NULL) THEN
+		WHEN (input_academic_year not in (Select AYcode from academic_year)) AND (input_academic_year is not NULL) THEN
 			SELECT 'invalid academic year' as 'message';
 			LEAVE sp;
 
-		WHEN (input_Fcode not in (Select Fcode from faculty)) AND (input_Fcode is not NULL) THEN
+		WHEN (input_faculty not in (Select Fcode from faculty)) AND (input_faculty is not NULL) THEN
 			SELECT 'invalid faculty' as 'message';
 			LEAVE sp;
 			
 		-- Check relationship exist in database or not
 		WHEN NOT EXISTS (SELECT AYcode, Fcode FROM ay_fac
-						WHERE AYcode = input_AYcode AND Fcode = input_Fcode) AND (status_case = 'delete')
+						WHERE AYcode = input_academic_year AND Fcode = input_faculty) AND (status_case = 'delete')
 			THEN SELECT 'no' as 'valid', 'The relationship between AYcode, Fcode do not exist in database' as 'message';
 			LEAVE sp;
         
 		-- Case when Delete 
 		WHEN (status_case = 'delete') THEN
 			SET @test = CONCAT("DELETE FROM ay_fac 
-								WHERE AYcode = '", input_AYcode,"' AND Fcode = '", input_Fcode,"';");
+								WHERE AYcode = '", input_academic_year,"' AND Fcode = '", input_faculty,"';");
                             
 		PREPARE stmt FROM @test;
         EXECUTE stmt;
@@ -35,7 +35,7 @@ sp: BEGIN
     
     -- Check the already in database relationship added to database
 		 WHEN EXISTS (SELECT AYcode, Fcode FROM ay_fac
-							WHERE AYcode = input_AYcode AND Fcode = input_Fcode) 
+							WHERE AYcode = input_academic_year AND Fcode = input_faculty) 
 								AND (status_case = 'insert')
 	 	THEN SELECT 'no' as 'valid', 'The relationship is already contained in database' as 'message';
 			LEAVE sp;
@@ -43,7 +43,7 @@ sp: BEGIN
     -- Case when Insert
 		WHEN (status_case = 'insert') THEN
 			SET @test = CONCAT("INSERT INTO ay_fac (AYcode, Fcode)
-								VALUES ('", input_AYcode,"', '", input_Fcode,"');");
+								VALUES ('", input_academic_year,"', '", input_faculty,"');");
                             
 			PREPARE stmt FROM @test;
 			EXECUTE stmt;
